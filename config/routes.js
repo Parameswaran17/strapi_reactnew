cfdaba55a40600d76edb991b330e9629e2250f89
Parameswaran17 <parameshwaran497@gmail.com>
2024-07-12 13:14:04 +0530

    added chnages

---
+++ config/routes.js
@@ -0,0 +1,23 @@
+module.exports = {
+    routes: [
+      {
+        method: 'GET',
+        path: '/articles',
+        handler: 'article.find',
+        config: {
+          policies: [],
+          middlewares: [],
+        },
+      },
+      {
+        method: 'GET',
+        path: '/articles/:id',
+        handler: 'article.findOne',
+        config: {
+          policies: [],
+          middlewares: [],
+        },
+      },
+    ],
+  };
+  
